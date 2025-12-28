321b7cd1427c765d8ac3c029fc24f004bc29c01f
Sakthi Priya <priya.krish051@gmail.com>
2025-12-28 22:46:26 +0530

    Add files via upload

---
+++ PythonBasics/LogAnalysis/output.py
@@ -0,0 +1,16 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: D:/Sakthi/PYTHON/fromIDLE/StuLogAna.py ===============
+Invalid log skipped: INVALID | DATA | TEST (Invalid format)
+STUDENT ACTIVITY REPORT
+
+S101 | Asha | Logins: 2 | Submissions: 0
+S102 | Ravi | Logins: 0 | Submissions: 1
+
+ABNORMAL BEHAVIOR REPORT
+S101 has multiple logins without logout
+
+DAILY ACTIVITY STATISTICS
+2025-03-10: 3 activities
+2025-03-11: 1 activities
