434498a3503509fc1f603927dea3cca9c057248d
Sakthi Priya <priya.krish051@gmail.com>
2025-12-28 22:39:24 +0530

    Add files via upload

---
+++ PythonBasics/PerformanceAnalysis/output.py
@@ -0,0 +1,43 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: D:/Sakthi/PYTHON/fromIDLE/AcaPerOfStu.py ==============
+STUDENT PERFORMANCE REPORT
+--------------------------------------------------
+Student ID     : S101
+Name           : Asha
+Assignment     : 78
+Test Score     : 80
+Average Score  : 79.0
+Attendance     : 92 %
+Study Hours    : 8 hrs/week
+Risk Level     : Low Risk
+--------------------------------------------------
+Student ID     : S102
+Name           : Ravi
+Assignment     : 65
+Test Score     : 68
+Average Score  : 66.5
+Attendance     : 85 %
+Study Hours    : 5 hrs/week
+Risk Level     : Moderate Risk
+--------------------------------------------------
+Student ID     : S103
+Name           : Meena
+Assignment     : 88
+Test Score     : 90
+Average Score  : 89.0
+Attendance     : 96 %
+Study Hours    : 10 hrs/week
+Risk Level     : Low Risk
+--------------------------------------------------
+Student ID     : S104
+Name           : Kiran
+Assignment     : 55
+Test Score     : 58
+Average Score  : 56.5
+Attendance     : 78 %
+Study Hours    : 4 hrs/week
+Risk Level     : High Risk
+Academic Support : REQUIRED
+--------------------------------------------------
